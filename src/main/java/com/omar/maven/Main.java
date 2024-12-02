8f3422a825b28445458ca2256ec9b71ed770a89c
OuBnOu7 <89420647+OuBnOu7@users.noreply.github.com>
2024-12-02 16:19:18 +0100

    First Commit

---
+++ src/main/java/com/omar/maven/Main.java
@@ -0,0 +1,11 @@
+package com.omar.maven;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+    }
+    public static int addition(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/omar/maven/MainTest.java
@@ -0,0 +1,17 @@
+package com.omar.maven;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class MainTest {
+    @Test
+    public void testAddition() {
+        assertEquals(2, Main.addition(1, 1));
+        assertNotEquals(3, Main.addition(1, 1));
+    }
+
+
+
+}
